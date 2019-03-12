20cb0019124e8b90cba7c6b03fa8b474ed65ad08
Andrew Ahnn <andrewahnn@gmail.com>
2019-03-11 22:07:00 -0400

    Add comments

--- scraper.py
+++ scraper.py
@@ -2,6 +2,16 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
+"""Check if a certain school has the requested course"""
+def check_school(school_code):
+    post_req = requests.post('https://banweb.banner.vt.edu/ssb/prod/hzsktgid.P_ProcChoices', 
+        data={'inst_subj': 'CS', 'school_sbgi_code': school_code, school_code: 'SUBMIT'})
+    post_req.raise_for_status()
+
+    post_soup = BeautifulSoup(post_req.content, 'html5lib')
+
+"""Start Script"""
+# Get a list of school codes, needed to check if a school has a certain course
 get_req = requests.get('https://banweb.banner.vt.edu/ssb/prod/hzsktgid.P_DispTranGuide')
 get_req.raise_for_status()
 
@@ -10,11 +20,6 @@ schools = get_soup.select('select[name="school_sbgi_code"] option')
 codes = []
 for tag in schools:
     codes.append(tag.get('value'))
-print(codes)
 
-def check_school(school_code):
-    post_req = requests.post('https://banweb.banner.vt.edu/ssb/prod/hzsktgid.P_ProcChoices', 
-        data={'inst_subj': 'CS', 'school_sbgi_code': school_code, school_code: 'SUBMIT'})
-    post_req.raise_for_status()
 
-    post_soup = BeautifulSoup(post_req.content, 'html5lib')
+
