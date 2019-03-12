97762c396237c3a128468eeda6093dcc076b5989
Andrew Ahnn <andrewahnn@gmail.com>
2019-03-12 09:46:29 -0400

    Complete basic functionality for check_school

--- scraper.py
+++ scraper.py
@@ -3,15 +3,28 @@ import requests
 import re
 
 """Check if a certain school has the requested course."""
-def check_school(school_code, requested_subject, requested_course):
+def check_school(school_code, requested_subject, requested_courseid=None):
     post_req = requests.post('https://banweb.banner.vt.edu/ssb/prod/hzsktgid.P_ProcChoices', 
         data={'inst_subj': requested_subject, 'school_sbgi_code': school_code, 'vt_btn': 'SUBMIT'})
-    post_req.raise_for_status()
+    if post_req.status_code == requests.codes.ok:
+        post_soup = BeautifulSoup(post_req.content, 'html5lib')
+        courses = post_soup.select('table.dataentrytable tbody tr td b')
 
-    post_soup = BeautifulSoup(post_req.content, 'html5lib')
+        subject_courses = []
+        for course in courses:
+            course_string = course.getText()
+            if requested_subject in course_string:
+                subject_courses.append(course_string)
+        if requested_courseid:
+            for subject_course in subject_courses:
+                if requested_courseid in subject_course:
+                    return subject_course
+            return None
+
+        return subject_courses
 
 """Start Script"""
-# Get a list of school codes, needed to check if a school has a certain course
+Get a list of school codes, needed to check if a school has a certain course
 get_req = requests.get('https://banweb.banner.vt.edu/ssb/prod/hzsktgid.P_DispTranGuide')
 get_req.raise_for_status()
 
@@ -21,6 +34,7 @@ codes = []
 for tag in schools:
     codes.append(tag.get('value'))
 
-# TODO: Add sys args functionality, e.g. scraper.py CS 1114.. make this case insensitive
-# TODO: Finish check_school function
+
+# TODO: Add sys args functionality, e.g. scraper.py CS 1114.. make this case insensitive, make it work with only subject arg
+# TODO: Make check_school only check VT courses
 # TODO: Add pauses between requests
