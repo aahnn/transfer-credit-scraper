fe56465f0919cce9aa835bd375aa0f3a2fd98801
Andrew Ahnn <andrewahnn@gmail.com>
2019-03-11 22:02:40 -0400

    Finish getting all school codes

--- scraper.py
+++ scraper.py
@@ -1,13 +1,16 @@
 from bs4 import BeautifulSoup
 import requests
-
+import re
 
 get_req = requests.get('https://banweb.banner.vt.edu/ssb/prod/hzsktgid.P_DispTranGuide')
 get_req.raise_for_status()
 
 get_soup = BeautifulSoup(get_req.content, 'html5lib')
 schools = get_soup.select('select[name="school_sbgi_code"] option')
-
+codes = []
+for tag in schools:
+    codes.append(tag.get('value'))
+print(codes)
 
 def check_school(school_code):
     post_req = requests.post('https://banweb.banner.vt.edu/ssb/prod/hzsktgid.P_ProcChoices', 
@@ -15,5 +18,3 @@ def check_school(school_code):
     post_req.raise_for_status()
 
     post_soup = BeautifulSoup(post_req.content, 'html5lib')
-
-check_school(3)
